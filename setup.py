a7301d690fe108c296ca7aa7e754c79b1241ac05
RsXm <rsxm@users.noreply.github.com>
2020-06-27 15:06:24 +0200

    Refactor project structure

--- setup.py
+++ setup.py
@@ -1,97 +1,40 @@
-"""A setuptools based setup module.
-See:
-https://packaging.python.org/en/latest/distributing.html
-"""
+from setuptools import setup
 
-from codecs import open
-from os import chdir, pardir, path
-
-from setuptools import find_packages, setup
-
-here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open('README.rst', 'r') as f:
     long_description = f.read()
-
-# allow setup.py to be run from any path
-chdir(path.normpath(path.join(path.abspath(__file__), pardir)))
 
 setup(
     name='dstack-bot',
-
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
-    # version=version_string,
     version='1.1.6',
-
     description=(
         'Simple bot that allows you to run commands on your server using invoke'),
-
     long_description=long_description,
-
-    # The project's main homepage.
     url='https://github.com/obitec/dstack-bot',
-
-    # Author details
-    author='JR Minnaar',
+    author='J Minnaar',
     author_email='jr.minnaar+pypi@gmail.com',
-
-    # Choose your license
     license='MIT License',
-
-    # See https://pypi.python.org/pypi?:action=list_classifiers
+    keywords='automation deploy telegram bots server administration',
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
         'Development Status :: 5 - Production/Stable',
-
-        # Indicate who your project is intended for
         'Intended Audience :: System Administrators',
-
-        #  Topics
-        # 'Topic :: System :: Installation/Setup',
-
-        # Environment
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: System :: Installation/Setup',
+        'Topic :: System :: Software Distribution',
         'Operating System :: OS Independent',
-
-        # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: MIT License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
-
-    # What does your project relate to?
-    keywords='automation deploy telegram bots server administration',
-
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['docs', 'tests']),
+    packages=['dstack_bot', ],
     include_package_data=True,
     zip_safe=True,
-
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
         'invoke',
         'python-dotenv',
         'python-telegram-bot',
     ],
-
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). You can install these using the following syntax,
-    # for example:
-    # $ pip install -e .[dev,test]
     extras_require={
         'dev': [
             'sphinx',
@@ -99,16 +42,10 @@ setup(
             'wheel',
         ],
         'stats': [
-            'psutil',
+            'psutil',  # easier to install with conda
         ]
     },
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
     entry_points={
-        'console_scripts': [
-            'dbot = dstack_bot.main:main'
-        ],
-    },
+        'console_scripts': ['dbot = dstack_bot.main:main']
+    }
 )
